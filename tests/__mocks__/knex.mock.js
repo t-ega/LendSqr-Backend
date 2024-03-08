3418eee54caaac27e0c134871825a673cb1b5a39
Tega <beniyoke@gmail.com>
2024-03-08 15:33:25 +0100

    Upddate tsconfig

--- tests/__mocks__/knex.mock.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// __mocks__/knex.js
-Object.defineProperty(exports, "__esModule", { value: true });
-const mockKnex = {
-    transaction: jest.fn((x) => x),
-};
-exports.default = mockKnex;
